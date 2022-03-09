9c1523b09ed209ef0a49f841cbd4b849c4941750
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-09 15:02:43 +0330

    add modal with bootstrap react

--- ex1/src/Components/TodoList.js
+++ ex1/src/Components/TodoList.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import AddTask from '../modals/AddTask';
 
 class TodoList extends React.Component {
 constructor(props) {
     super(props);
 
     this.state = {
+
     };
 }
 
     render() {
         return (
-            <div className='header text-center'>
-                <h3>Todo List</h3>
-                <Button variant='primary'>Create Task</Button>
+            <div className='header text-center p-5 w-100'>
+                <h1>ToDo List</h1>
+                <AddTask />
             </div>
         );
     }
