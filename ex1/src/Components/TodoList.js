be6b5b93a3fb2c38f513afb53f9a6c7c41b220af
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-10 02:43:13 +0330

    check edite

--- ex1/src/Components/TodoList.js
+++ ex1/src/Components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card} from 'react-bootstrap';
-import {FaTrashAlt , FaEdit} from 'react-icons/fa'
+import {FaTrashAlt , FaEdit} from 'react-icons/fa';
+
 
 class TodoList extends React.Component {
     render() {
